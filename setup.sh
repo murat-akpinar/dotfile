0401b9fe784b379efe25d47211a90408cef85cf7
magwyen <akpinarmurat@protonmail.com>
2023-04-15 16:14:09 +0000

    “yetki”

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+mkdir ~/.poshthemes
+wget https://raw.githubusercontent.com/magwyen/dotfile/main/mytheme.omp.json -P ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+echo 'eval "$(oh-my-posh init bash)"' >> .profile
+echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/mytheme.omp.json)"' >> .profile
+oh-my-posh font install
