73404a8fae172f29f538fb15fb3e5ec45beea6a6
Murat Akpınar <112278587+magwyen@users.noreply.github.com>
2023-04-17 20:45:04 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
-wget https://raw.githubusercontent.com/magwyen/dotfile/main/mytheme.omp.json -P ~/.poshthemes
+cp ./mytheme.omp.json ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.omp.*
 echo 'eval "$(oh-my-posh init bash)"' >> ~/.profile
 echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/mytheme.omp.json)"' >> ~/.profile
