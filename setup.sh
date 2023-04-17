df217ff0d78bffb51f590033a3874a5417a9c5c2
Murat Akpınar <112278587+magwyen@users.noreply.github.com>
2023-04-17 19:39:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
 wget https://raw.githubusercontent.com/magwyen/dotfile/main/mytheme.omp.json -P ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.omp.*
-echo 'eval "$(oh-my-posh init bash)"' >> .profile
-echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/mytheme.omp.json)"' >> .profile
-echo 'eval "$(oh-my-posh init bash)"' >> .bashrc
-echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/mytheme.omp.json)"' >> .bashrc
+echo 'eval "$(oh-my-posh init bash)"' >> ~/.profile
+echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/mytheme.omp.json)"' >> ~/.profile
+echo 'eval "$(oh-my-posh init bash)"' >> ~/.bashrc
+echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/mytheme.omp.json)"' >> ~/.bashrc
 oh-my-posh font install
