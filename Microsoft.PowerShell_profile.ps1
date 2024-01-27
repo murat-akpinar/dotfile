32f8b11a7677fb1800e28a49ec7ec7d2b44eddf9
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-01-27 18:49:31 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
-oh-my-posh init pwsh --config '~\Documents\WindowsPowerShell\tema.json' | Invoke-Expression
+oh-my-posh init pwsh --config '~\Documents\WindowsPowerShell\mytheme.omp.json' | Invoke-Expression
 Import-Module -Name Terminal-Icons
