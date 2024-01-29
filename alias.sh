71f25d8234431055b094319e27f8807cfaa49111
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-01-29 15:34:10 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,6 +1,5 @@
 echo "alias ll='ls -lvrt'" >> ~/.profile
 echo "alias cdd='cd ..'" >> ~/.profile
-echo "alias catt='cat'" >> ~/.profile
-echo "alias cat='batcat'" >> ~/.profile
+echo "alias bcat='batcat'" >> ~/.profile
 
 source ~/.profile
