22115b16557064a63774ad154a74510d7b1c41f2
murat-akpinar <akpinarmurat@protonmail.com>
2024-01-30 16:03:43 +0300

    yeni alias du -sh

--- alias.sh
+++ alias.sh
@@ -1,6 +1,9 @@
 echo "alias ll='ls -lvrt'" >> ~/.profile
+
 echo "alias cdd='cd ..'" >> ~/.profile
 echo "alias cddd='cd ../..'" >> ~/.profile
+
+echo "alias du='du -sh'" >> ~/.profile
 
 echo "alias bcat='batcat'" >> ~/.profile
 echo "alias ara='find . -print0 | xargs -0 stat -c "%Z %z %n" | sort -nr | head -10'" >> ~/.profile
