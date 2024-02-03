b2f8f2f6b321d8e7c2e64ae9a8f07e8748c4046d
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-02-04 02:39:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
 cp ./mytheme.omp.json ~/.poshthemes
+cp ~/Downloads/tema/dotfile/mytheme.omp.json ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.omp.*
 
 echo 'eval "$(oh-my-posh init bash)"' >> ~/.bashrc
