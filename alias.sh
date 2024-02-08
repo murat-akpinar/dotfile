08294d85d4bb0a4518ada1a87379fa9cd2f78a6e
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-02-08 19:20:38 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -10,6 +10,13 @@ echo "alias du='du -sh *'" >> ~/.bashrc
 echo "alias bcat='batcat'" >> ~/.bashrc
 echo "alias chng='find . -print0 | xargs -0 stat -c "%Z %z %n" | sort -nr | head -10'" >> ~/.bashrc
 
+#### DOCKER #####
+alias dc='docker compose'
+alias di='docker images'
+alias dcdel='docker container rm $(docker container ls -aq)'
+#### DOCKER ####
+
+
 
 source ~/.bashrc
 
