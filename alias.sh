fce1f7b538c9d7156654d25bfa5c183c2f897075
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-02-11 12:48:19 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,22 +1,62 @@
-echo "alias ll='ls -lvrt'" >> ~/.bashrc
+cat <<EOL >> ~/.bashrc
 
-echo "alias cdd='cd ..'" >> ~/.bashrc
-echo "alias cddd='cd ../..'" >> ~/.bashrc
+alias ll='ls -lvrt'
 
+alias cdd='cd ..'
+alias cddd='cd ../..'
 
-echo "alias du='du -sh *'" >> ~/.bashrc
-
-
-echo "alias bcat='batcat'" >> ~/.bashrc
-echo "alias chng='find . -print0 | xargs -0 stat -c "%Z %z %n" | sort -nr | head -10'" >> ~/.bashrc
+alias du='du -sh *'
+alias bcat='batcat'
+alias chng='find . -print0 | xargs -0 stat -c "%Z %z %n" | sort -nr | head -10'
 
 #### DOCKER #####
+
 alias dc='docker compose'
 alias di='docker images'
 alias dcdel='docker container rm $(docker container ls -aq)'
+
+#belirtilen netwwork ismine bağlı networkleri listeler
+alias dn="docker network inspect --format='{{range .Containers}} {{.Name}} {{end}}'"
+#belirtilen container'in ip adresini verir
+alias dip="docker inspect -f '{{range .NetworkSettings.Networks}} {{.IPAddress}}{{end}}'"
+
 #### DOCKER ####
 
+#### K8S ####
+alias k='kubectl'
+alias kns='kubectl config set-context --current --namespace'
 
+
+alias ks='kubectl -n kube-system'
+alias kcf='kubectl create -f'
+alias kaf='kubectl apply -f'
+
+alias kn='kubectl get nodes'
+alias kp='kubectl get pods'
+
+alias kpedit='kubectl edit pods'
+alias kpdes='kubectl describe pod'
+alias kgp='kubectl get pods'
+alias kpall='kubectl get pods -A'
+
+alias kexp='kubectl explain'
+alias klog='kubectl logs'
+alias kall='kubectl get all'
+
+alias kpvc='kubectl get pvc'
+alias ksvc='kubectl get svc'
+alias kgs='kubectl get services'
+alias kgd='kubectl get deployments'
+
+alias krm='kubectl delete'
+alias krmf='kubectl delete -f'
+alias kdp='kubectl delete pod'
+alias kds='kubectl delete service'
+alias kdd='kubectl delete deployment'
+alias kdn='kubectl delete namespace'
+alias knewtoken='kubeadm token create --print-join-command'
+#### K8S ####
+EOL
 
 source ~/.bashrc
 
