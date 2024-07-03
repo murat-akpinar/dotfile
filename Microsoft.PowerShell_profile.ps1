75ba8debeb5e013af1d36c94cabff1b2ca182aa9
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-07-03 07:38:21 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 oh-my-posh init pwsh --config '~\Documents\WindowsPowerShell\mytheme.omp.json' | Invoke-Expression
 Import-Module -Name Terminal-Icons
+
+function ssh { 
+    & "C:\Windows\System32\OpenSSH\ssh.exe" -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" $args 
+}
