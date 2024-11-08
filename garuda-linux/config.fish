5ee3356ee37c2908b266296884f0993883b4b753
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-11-08 22:34:20 +0300

    Update config.fish

--- garuda-linux/config.fish
+++ garuda-linux/config.fish
@@ -8,7 +8,7 @@ set -x SHELL /usr/bin/fish
 
 ## Export variable need for qt-theme
 if type "qtile" >> /dev/null 2>&1
-   set -x QT_QPA_PLATFORMTHEME "qt5ct"
+set -x QT_QPA_PLATFORMTHEME "qt5ct"
 end
 
 # Set settings for https://github.com/franciscolourenco/done
@@ -19,27 +19,27 @@ set -U __done_notification_urgency_level low
 ## Environment setup
 # Apply .profile: use this to put fish compatible .profile stuff in
 if test -f ~/.fish_profile
-  source ~/.fish_profile
+source ~/.fish_profile
 end
 
 # Add ~/.local/bin to PATH
 if test -d ~/.local/bin
-    if not contains -- ~/.local/bin $PATH
-        set -p PATH ~/.local/bin
-    end
+if not contains -- ~/.local/bin $PATH
+set -p PATH ~/.local/bin
+end
 end
 
 # Add depot_tools to PATH
 if test -d ~/Applications/depot_tools
-    if not contains -- ~/Applications/depot_tools $PATH
-        set -p PATH ~/Applications/depot_tools
-    end
+if not contains -- ~/Applications/depot_tools $PATH
+set -p PATH ~/Applications/depot_tools
+end
 end
 
 
 ## Starship prompt
 if status --is-interactive
-   source ("/usr/bin/starship" init fish --print-full-init | psub)
+source ("/usr/bin/starship" init fish --print-full-init | psub)
 end
 
 
@@ -50,66 +50,66 @@ end
 ## Functions
 # Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
 function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+switch (commandline -t)
+case "!"
+commandline -t $history[1]; commandline -f repaint
+case "*"
+commandline -i !
+end
 end
 
 function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
+switch (commandline -t)
+case "!"
+commandline -t ""
+commandline -f history-token-search-backward
+case "*"
+commandline -i '$'
+end
 end
 
 if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
+bind -Minsert ! __history_previous_command
+bind -Minsert '$' __history_previous_command_arguments
 else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
 end
 
 # Fish command history
 function history
-    builtin history --show-time='%F %T '
+builtin history --show-time='%F %T '
 end
 
 function backup --argument filename
-    cp $filename $filename.bak
+cp $filename $filename.bak
 end
 
 # Copy DIR1 DIR2
 function copy
-    set count (count $argv | tr -d \n)
-    if test "$count" = 2; and test -d "$argv[1]"
-	set from (echo $argv[1] | string trim --right --chars=/)
-	set to (echo $argv[2])
-        command cp -r $from $to
-    else
-        command cp $argv
-    end
+set count (count $argv | tr -d \n)
+if test "$count" = 2; and test -d "$argv[1]"
+set from (echo $argv[1] | string trim --right --chars=/)
+set to (echo $argv[2])
+command cp -r $from $to
+else
+command cp $argv
+end
 end
 
 # Cleanup local orphaned packages
 function cleanup
-    while pacman -Qdtq
-        sudo pacman -R (pacman -Qdtq)
-    end
+while pacman -Qdtq
+sudo pacman -R (pacman -Qdtq)
+end
 end
 
 ## Useful aliases
 
 # Replace ls with eza
-alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
+alias ll 'eza -al --color=always --group-directories-first --icons' # preferred listing
 alias la 'eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll 'eza -l --color=always --group-directories-first --icons'  # long format
+alias ls 'eza -l --color=always --group --icons'  # long format
 alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
 alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 
@@ -168,7 +168,7 @@ alias rip 'expac --timefmt="%Y-%m-%d %T" "%l\t%n %v" | sort | tail -200 | nl'
 # Run fastfetch if session is interactive
 if status --is-interactive && type -q fastfetch
 #    fastfetch --load-config dr460nized
-    fastfetch --load-config dracula
+fastfetch --load-config dracula
 end
 
 alias wg-up='sudo wg-quick up wg0'
@@ -181,8 +181,9 @@ alias du='du -sh *'
 alias bcat='bat'
 alias chng='find . -print0 | xargs -0 stat -c "%Z %z %n" | sort -nr | head -10'
 
-alias outtar="tar -xzvf"
-alias inntar="tar -cvf"
-
 alias lastf="find . -type f -printf '%TY-%Tm-%Td %TH:%TM %p\n' | sort -r | head -n 10"
+alias port-find='ss -tuln | grep'
 
+# DOCKER Alias #
+#
+alias d='docker'
